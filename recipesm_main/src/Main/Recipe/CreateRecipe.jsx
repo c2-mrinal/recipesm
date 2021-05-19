1ec150a5eac1bebf63d38a57a7e6f09a12e32640
c2-mrinal <mrinalspec@gmail.com>
2021-05-20 01:34:22 +0530

    create functionality added

--- recipesm_main/src/Main/Recipe/CreateRecipe.jsx
+++ recipesm_main/src/Main/Recipe/CreateRecipe.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import './createRecipe.css'
+import {AiOutlineClose} from 'react-icons/ai';
 
 
 function CreateRecipe() {
@@ -14,10 +15,41 @@ const [Nutrition, setNutrition] = useState({nutrent:'',
                                                                     portion:''})
 const [Time, setTime] = useState({active:'',
                                                         total:'',
-                                                        yield:''})
-                            console.log('yyy',recipeHead);
+                                                        yield:''});
+const [TotalIngredients, setTotalIngredients] = useState({});
+
+                            // console.log('yyy',recipeHead);
+
     let  addNewData = (e)=>{
+            var value= e.target.value;
+            if (value == 'Ingredient'){
+                var ingred ={[Ingredient.item]:Ingredient.amount};
+                setTotalIngredients({...TotalIngredients,...ingred})
+                setIngredient({item:'',amount:''})
+
+            }
+            // console.log('wwww',e.target.value);
+    }
+    let editValue=(e)=>{
+        const {value,name} =e.target;
         
+        console.log('work', e.target);
+        if(name === 'TotalIngredients'){
+            console.log(value);
+        }
+
+    }
+    let deleteValue = (e)=>{
+        let {name,value} = e.target;
+        var xxx = TotalIngredients;
+        if(name === 'TotalIngredients'){
+           Object.keys( TotalIngredients).map(del=>{
+                if(del == value){
+                   delete xxx[del];
+                }
+            })
+            setTotalIngredients({...xxx});
+        }
     }
     return (
         <div>
@@ -29,12 +61,12 @@ const [Time, setTime] = useState({active:'',
             <div>
                         <div className="form__group field">
                                 <input type="input" className="form__field" placeholder="Name" name="name" id='name'  value={recipeHead.recipeName} 
-                                onChange={(e)=>setrecipeHead({recipeName:e.target.value})}/>
+                                onChange={(e)=>setrecipeHead({...recipeHead,recipeName:e.target.value})}/>
                                 <label for="name" className="form__label">Recipe Name</label>
                         </div>
                         <div className="form__group field">
                             <textarea type="input" className="form__field" placeholder="Name" name="name" id='name'   value={recipeHead.recipeDesciption} 
-                            onChange={(e)=>setrecipeHead({recipeDesciption:e.target.value})} ></textarea>
+                            onChange={(e)=>setrecipeHead({...recipeHead,recipeDesciption:e.target.value})} ></textarea>
                             <label for="name" className="form__label">About</label>
                         </div>    
             </div>
@@ -45,20 +77,32 @@ const [Time, setTime] = useState({active:'',
                     <section className='ingredientBox'>
                         <div className='addIngredient'>
                             <div className="form__group field">
-                                <input type="input" className="form__field" placeholder="Name" name="name" id='name'  value={Ingredient.amount}
-                                onChange={(e)=>setIngredient({amount:e.target.value})}/>
+                                <input type="input" className="form__field" placeholder="Ingredient" name="item"  value={Ingredient.item}
+                                onChange={(e)=>setIngredient({...Ingredient,item:e.target.value})}/>
                                 <label for="name" className="form__label">Ingredient</label>
                             </div>
                             <div className="form__group field">
-                                <input type="input" className="form__field" placeholder="Name" name="name" id='name'  value={Ingredient.item}
-                                onChange={(e)=>setIngredient({item:e.target.value})}/>
+                                <input type="input" className="form__field" placeholder="amount" name="amount"   value={Ingredient.amount}
+                                onChange={(e)=>setIngredient({...Ingredient,amount:e.target.value})}/>
                                 <label for="name" className="form__label">Amount</label>
                             </div>    
                             <div className='addButton'>
-                                    <input type='button' value='ADD' className='addClickButton'/> 
+                                    <input type='button' value='Ingredient' className='addClickButton' onClick={addNewData}/> 
                             </div>
                         </div>
-                        <div className='addedIngredientBox'> </div>
+                        <div className='addedIngredientBox'>
+                            {TotalIngredients != {} &&
+                            Object.entries(TotalIngredients).map((t,id)=>{
+                                return(
+                                    <div className='inputCapsule' name='TotalIngredients' value={t} id='eeo' onClick={editValue}>
+                                    <div className='dataCapsule'>
+                                    <div>{t[0]}</div>
+                                     <div className='centerCapsule'>{t[1]}</div></div>
+                                    <div className='closeCapsule' ><button id={id} name='TotalIngredients' value={t[0]} onClick={deleteValue} >X</button></div>
+                                    </div>
+                                )
+                         })}
+                        </div>
                     </section>
             </fieldset>
 
@@ -68,16 +112,16 @@ const [Time, setTime] = useState({active:'',
                     <div className='addIngredient'>
                         <div className="form__group field">
                             <input type="input" className="form__field" placeholder="Name" name="name" id='name'  value={Equipment.utensil} 
-                            onChange={(e)=>setEquipment({utensil:e.target.value})}/>
+                            onChange={(e)=>setEquipment({...Equipment,utensil:e.target.value})}/>
                             <label for="name" className="form__label">Equipment</label>
                         </div>
                         <div className="form__group field">
                             <input type="input" className="form__field" placeholder="Name" name="name" id='name'  value={Equipment.size} 
-                            onChange={(e)=>setEquipment({size:e.target.value})}/>
+                            onChange={(e)=>setEquipment({...Equipment,size:e.target.value})}/>
                             <label for="name" className="form__label">Size</label>
                         </div>    
                         <div className='addButton'>
-                            <input type='button' value='ADD' className='addClickButton'/> 
+                            <input type='button' value='Equipment' className='addClickButton' onClick={addNewData} /> 
                         </div>
                     </div>
                     <div className='addedIngredientBox'> </div>
@@ -89,7 +133,7 @@ const [Time, setTime] = useState({active:'',
             <section className='preperationBox'>
                 <div className='prepHeader'>
                     <div> <h2>Preperation step</h2></div>
-                    <div><input type='button' value='Add Step' className=''/> </div>
+                    <div><input type='button' value='Add Step' className='' onClick={addNewData} /> </div>
                 </div>
 
                 <div className='eachPrepStep'>
@@ -108,12 +152,12 @@ const [Time, setTime] = useState({active:'',
                 <div className='timeDetail'>
                     <div className="form__group field">
                         <input type="input" className="form__field" placeholder="Name" name="name" id='name'  value={Time.active}
-                        onChange={(e)=>setTime({active:e.target.value})} />
+                        onChange={(e)=>setTime({...Time,active:e.target.value})} />
                         <label for="name" className="form__label">Active Time</label>
                     </div>
                     <div className="form__group field">
                         <input type="input" className="form__field" placeholder="Name" name="name" id='name'  value={Time.total} 
-                        onChange={(e)=>setTime({active:e.target.value})}/>
+                        onChange={(e)=>setTime({...Time,active:e.target.value})}/>
                         <label for="name" className="form__label">Preperation Time</label>
                     </div> 
                 </div>
@@ -125,16 +169,16 @@ const [Time, setTime] = useState({active:'',
                 <div className='addIngredient'>
                     <div className="form__group field">
                 <input type="input" className="form__field" placeholder="Name" name="name" id='name'  value={Nutrition.nutrent} 
-                onChange={(e)=>setNutrition({nutrent:e.target.value})}/>
+                onChange={(e)=>setNutrition({...Nutrition,nutrent:e.target.value})}/>
                 <label for="name" className="form__label">Nutrition Name</label>
                 </div>
                     <div className="form__group field">
                 <input type="input" className="form__field" placeholder="Name" name="name" id='name'  value={Nutrition.portion} 
-                onChange={(e)=>setNutrition({portion:e.target.value})} />
+                onChange={(e)=>setNutrition({...Nutrition,portion:e.target.value})} />
                 <label for="name" className="form__label">Unit</label>
                 </div>    
                 <div className='addButton'>
-                <input type='button' value='ADD' className='addClickButton'/> 
+                <input type='button' value='ADD' className='addClickButton' onClick={addNewData} /> 
                 </div>
                 </div>
                 <div className='addedIngredientBox'> </div>
@@ -147,7 +191,7 @@ const [Time, setTime] = useState({active:'',
                 <div className='yieldDetail'>
                     <div className="form__group field">
                 <input type="input" className="form__field" placeholder="Name" name="name" id='name' value={Time.yield}
-                onChange={(e)=>setTime({active:e.target.value})} />
+                onChange={(e)=>setTime({...Time,active:e.target.value})} />
                 <label for="name" className="form__label">Yield Detail</label>
                 </div>
 
