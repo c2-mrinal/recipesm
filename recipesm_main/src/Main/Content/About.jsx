093a1be2e70467e08fa44541cbff2e4448815abb
Shruti Chaudhari <shruti@magentaconnect.com>
2021-05-23 13:29:40 +0530

    remove react folder

--- recipesm_main/src/Main/Content/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function About() {
-    return (
-        <div style={{backgroundColor:' Purple', width:'100%', height:'100%'}}>
-            About
-        </div>
-    )
-}
--- recipesm_main/src/Main/Content/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Contact() {
-    return (
-        <div style={{backgroundColor:' skyBlue', width:'100%', height:'100%'}}>
-            Contact
-        </div>
-    )
-}
